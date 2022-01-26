2343ac7fc4b9b89e8edafbc2f7fc5fddb1725eee
Chase Crowder <gcrowder93@gmail.com>
2022-01-25 17:13:15 -0800

    fixed failing deploy

--- src/services/excuses.js
+++ src/services/excuses.js
@@ -1,6 +1,5 @@
 export async function getExcuses() {
   const response = await fetch(`https://excuser.herokuapp.com/v1/excuse/10`);
   const excuseData = await response.json();
-  console.log('excuseData', excuseData);
   return excuseData;
 }
