21d344e7deca3cab525a14af6ad92966da6a96db
Chase Crowder <gcrowder93@gmail.com>
2022-01-26 14:26:58 -0800

    passing 2nd test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('Should render the header', async () => {
@@ -11,4 +12,22 @@ test('Should render the header', async () => {
   expect(head).toBeInTheDocument();
   expect(cat).toBeInTheDocument();
   expect(search).toBeInTheDocument();
+});
+
+test('we search for a category', async () => {
+  render(<App />);
+
+  const searchbar = await screen.findByRole('textbox', { search: /search:/i });
+  const excuseCategory = 'family';
+  userEvent.type(searchbar, excuseCategory);
+
+  const excuses = await screen.findAllByText(excuseCategory, { exact: false });
+
+  const result = excuses.map((excuses) => excuses.textContent);
+
+  const handleCategoryCheck = (excuses) => excuses.toLowerCase().includes(excuseCategory);
+
+  const isSameCat = result.every(handleCategoryCheck);
+
+  expect(isSameCat).toBe(true);
 });
